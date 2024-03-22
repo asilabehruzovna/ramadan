58da76181ce7a58dd8389ab5968c14345f2998b3
asilabehruzovna <asilabehruzovna@gmail.com>
2024-03-22 15:52:53 +0500

    ramadan

---
+++ script.js
@@ -0,0 +1,12 @@
+let text = document.getElementById('text');
+let img1 = document.getElementById('img1');
+let img2 = document.getElementById('img2');
+let img3 = document.getElementById('img3');
+let img4 = document.getElementById('img4');
+window.addEventListener('scroll' , function(){
+    let value = this.window.scrollY
+    text.style.marginTop = value * -1.5+'px';
+    img1.style.top = value * 0.75+'px';
+    img2.style.top = value * 0.5+'px';
+    img3.style.top = value * 0.25+'px';
+})
